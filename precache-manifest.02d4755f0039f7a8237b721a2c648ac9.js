be258620c9cd7d3ffc360a8a576fe7a332a18703
rawitP <rawit.p@ku.th>
2019-05-21 14:56:26 +0700

    Fix homepage in package.json

---
+++ precache-manifest.02d4755f0039f7a8237b721a2c648ac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1c2de4b1a6a6d3eaf984f8463c92e6",
+    "url": "/stat-visualization/index.html"
+  },
+  {
+    "revision": "bfdced99409dbf25b0d3",
+    "url": "/stat-visualization/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/stat-visualization/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bfdced99409dbf25b0d3",
+    "url": "/stat-visualization/static/js/main.ce809a2c.chunk.js"
+  },
+  {
+    "revision": "e24a2cdf4f9919e3daf3",
+    "url": "/stat-visualization/static/js/runtime~main.556f46a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42f70aa077b69e304f7b9418b1cfeb94.js"
+  "/stat-visualization/precache-manifest.02d4755f0039f7a8237b721a2c648ac9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stat-visualization/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
