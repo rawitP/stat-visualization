aa0f265d6d4c34047e361e885ddf0cc6a8003739
rawitP <rawit.p@ku.th>
2019-05-21 14:49:59 +0700

    First commit

---
+++ precache-manifest.42f70aa077b69e304f7b9418b1cfeb94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e22516ecdb330b2f732974cad2931f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d0b6965511213ac575b",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1d0b6965511213ac575b",
+    "url": "/static/js/main.4e01a2d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
