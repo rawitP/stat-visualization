ea193e29346a5f04e969bb3f38f960b59cfeba37
rawitP <rawit.p@ku.th>
2019-05-31 00:00:40 +0700

    Updates

---
+++ precache-manifest.81ee37fa9a087b7f908787e91b1ddc6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe544bc0a44e2a232e63bf1d7556512",
+    "url": "/stat-visualization/index.html"
+  },
+  {
+    "revision": "adebc8474ef2912e777d",
+    "url": "/stat-visualization/static/css/main.cbef72de.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/stat-visualization/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "adebc8474ef2912e777d",
+    "url": "/stat-visualization/static/js/main.28969c8d.chunk.js"
+  },
+  {
+    "revision": "e24a2cdf4f9919e3daf3",
+    "url": "/stat-visualization/static/js/runtime~main.556f46a3.js"
+  },
+  {
+    "revision": "ea9e06e2e62b6b0c090fd4f822baa439",
+    "url": "/stat-visualization/static/media/mdes_logo.ea9e06e2.png"
+  }
+]);
